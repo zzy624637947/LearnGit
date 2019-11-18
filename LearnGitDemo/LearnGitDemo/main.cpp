4c86a94b9ab0b8302aa45f2aa33b271e7dc6e386
zzy624637947 <zzy624637947@163.com>
2019-11-18 01:39:59 -0600

    First Commit

---
+++ LearnGitDemo/LearnGitDemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnGitDemo
+//
+//  Created by Zhiyuan Zhang on 11/18/19.
+//  Copyright © 2019 Zhiyuan Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
